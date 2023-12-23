cab17107590598e14f1275f9b0db69ecb6267ad6
Matt Clyde <me@mjclyde.com>
2023-12-22 21:27:16 -0700

    moved more options and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,6 +13,19 @@ vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
 vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
 vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
 vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
 vim.keymap.set("n", "B", "^")
 vim.keymap.set("n", "E", "$")
 vim.keymap.set("n", "<C-s>", ":wa<CR>")
@@ -34,3 +47,35 @@ vim.keymap.set("n", "zk", ":res +5<CR>", { silent = true })
 vim.keymap.set("n", "zj", ":res -5<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>gs", "<CMD>G<CR>")
+
+
+-- Telescope
+local telescopeBuiltIn = require('telescope.builtin')
+local telescopeThemes = require('telescope.themes')
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', telescopeBuiltIn.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', telescopeBuiltIn.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  telescopeBuiltIn.current_buffer_fuzzy_find(telescopeThemes.get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+local function telescope_live_grep_open_files()
+  telescopeBuiltIn.live_grep {
+    grep_open_files = true,
+    prompt_title = 'Live Grep in Open Files',
+  }
+end
+vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
+vim.keymap.set('n', '<leader>ss', telescopeBuiltIn.builtin, { desc = '[S]earch [S]elect Telescope' })
+vim.keymap.set('n', '<leader>gf', telescopeBuiltIn.git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>sf', telescopeBuiltIn.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', telescopeBuiltIn.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', telescopeBuiltIn.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', telescopeBuiltIn.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', telescopeBuiltIn.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', telescopeBuiltIn.resume, { desc = '[S]earch [R]esume' })
+
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,23 +2,48 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- Save undo history
+vim.o.undofile = true
 
-vim.opt.incsearch = true
+vim.o.incsearch = true
 
-vim.opt.termguicolors = true
+vim.o.colorcolumn = "100"
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+-- Set highlight on search
+vim.o.hlsearch = false
 
-vim.opt.updatetime = 50
+-- Make line numbers default
+vim.wo.number = true
 
-vim.opt.colorcolumn = "100"
+-- Enable mouse mode
+vim.o.mouse = 'a'
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.scrolloff = 8
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
